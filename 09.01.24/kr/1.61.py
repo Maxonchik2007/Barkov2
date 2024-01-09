5d8b826f07ce651c6c114e4e62738cc74ba2061a
student <454@ghg.hjh>
2024-01-09 13:25:01 +1200

    классная работа за 9.01.23

---
+++ 09.01.24/kr/1.61.py
@@ -0,0 +1,18 @@
+a = int(input())
+a2 = a * a
+a4 = a2 * a2
+print(" a в4 степени", a4)
+a2 = a * a
+a4 = a2 * a2
+a6 = a4 * a2
+print(" a в 6 степени", a6)
+a2 = a * a
+a4 = a2 * a2
+a6 = a4 * a2
+a7 = a6 * a
+print(" a в 7 степени", a7)
+a2 = a * a
+a4 = a2 * a2
+a8 = a4 * a4
+print("a в 8 степени",a8)
+
---
+++ 09.01.24/kr/1.py
@@ -0,0 +1,4 @@
+a, b, c = [int(i) for i in input().split()]
+print(a, '+', b, '+', c, '=', a + b + c)
+print(a, '*', b, '*', c, '=', a * b * c)
+print('(', a, '+', b, '+', c,')', '/3', '=', (a + b + c) / 3)
