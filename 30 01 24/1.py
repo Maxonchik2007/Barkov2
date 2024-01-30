dde8e102486fa44c6a4e1540c3f26c141f0ff8cf
student <454@ghg.hjh>
2024-01-30 13:12:27 +1200

    16/01/24

---
+++ 30 01 24/1.py
@@ -0,0 +1,10 @@
+m = 185
+c = 0
+for i in range(m//15):
+    for j in range(m//17):
+        for k in range(m//21):
+            if (i * 15 + j * 17 + k * 21) > 185:
+                break
+            elif (i * 15 + j * 17 + k * 21) == 185:
+                c += 1
+print(c)
